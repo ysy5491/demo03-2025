aec4cff1e8a065055ab4b379daf26698cd18f6bf
ysy5491 <a57524424@gmail.com>
2025-12-15 16:57:18 +0900

    csrf 설정 끄기
    restapi구현은 csrf 설정 끄기 지향하자
    h2 콘솔 접근을 위한 헤더 설정도 해줌

--- src/main/java/com/ll/demo03/global/security/SecurityConfig.java
+++ src/main/java/com/ll/demo03/global/security/SecurityConfig.java
@@ -9,7 +9,8 @@ import org.springframework.security.web.SecurityFilterChain;
 public class SecurityConfig {
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.authorizeRequests(authorizeRequests ->
+        http
+                .authorizeRequests(authorizeRequests ->
                         authorizeRequests.requestMatchers("/h2-console/**") // 이 부분은
                                 .permitAll() // 허용
                                 .requestMatchers("/actuator/**")
@@ -17,9 +18,18 @@ public class SecurityConfig {
                                 .anyRequest() // 어떤 요청에도
                                 .authenticated() // 인증되야한다
 
-        ).formLogin(formLogin -> formLogin
+                ).headers( // h2 콘솔 들어가기위한 설정
+                        headers ->
+                                headers.frameOptions(
+                                        frameOptions ->
+                                                frameOptions.sameOrigin()
+                                )
+                ).csrf( // csrf옵션 끄기(restAPI 방식에서는 끄자, 타임리프,mpa 방식은 키자)
+                        csrf ->
+                                csrf.disable()
+                ).formLogin(formLogin -> formLogin
                         .permitAll()
-        );
+                );
         return http.build();
     }
 }
