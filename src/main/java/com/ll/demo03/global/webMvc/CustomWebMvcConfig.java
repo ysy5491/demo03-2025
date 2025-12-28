b06b9f8edff9e6e92429137935e016a272dafcd6
ysy5491 <a57524424@gmail.com>
2025-12-28 19:52:05 +0900

    프론트와 통신을 위해 cors허용 설정

---
+++ src/main/java/com/ll/demo03/global/webMvc/CustomWebMvcConfig.java
@@ -0,0 +1,21 @@
+package com.ll.demo03.global.webMvc;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class CustomWebMvcConfig implements WebMvcConfigurer {
+
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
+        registry.addMapping("/api/**")
+                .allowedOrigins(
+                        "https://cdpn.io", // codepen.io에서 제공하는 프록시 서버 주소 추가(cors 오류 해결 위해서) 요청 허용
+                        "http://localhost:5173" // 프론트엔드 개발서버 주소 추가(cors 오류 해결 위해서) 요청 허용
+                )
+                .allowedMethods("*")
+                .allowedHeaders("*")
+                .allowCredentials(true);
+    }
+}
